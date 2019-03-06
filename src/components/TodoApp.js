92f20d4fb419b0c875afcfe71a09707b4d87dae5
nehasiwakoti <siwakotineha22@gmail.com>
2019-03-06 10:28:20 +0545

    setup app component

---
+++ src/components/TodoApp.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './TodoApp.css';
+
+class TodoApp extends Component {
+	render() {
+		return (
+			<div className="App">
+				<h1>Todo App</h1>
+			</div>
+		);
+	}
+}
+
+export default TodoApp;
